6a75d3763b4c73d781b04f2bc0681945b9b59fbe
Mostafa <mostafa.sedaghat@gmail.com>
2022-12-13 14:46:03 +0800

    feat(vcbc): defining proposal

--- src/mvba/mod.rs
+++ src/mvba/mod.rs
@@ -9,4 +9,13 @@ mod hash;
 mod vcbc;
 
 pub type NodeId = usize;
-pub type ProposalChecker = fn(&proposal::Proposal) -> bool;
+
+
+
+/// A proposed data with the proof inside. It is the same as $(w, π)$ in the spec.
+pub type Proposal = Vec<u8>;
+
+/// MessageValidity is same as &Q_{ID}$ ins spec: a global polynomial-time computable
+/// predicate QID known to all parties, which is determined by an external application.
+/// Each party may propose a value v together with a proof π that should satisfy QID .
+pub type MessageValidity = fn(NodeId, &Proposal) -> bool;
