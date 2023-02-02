d79eadba60d8f6b1de71388c57ff7a00d8ff572c
Mostafa <mostafa.sedaghat@gmail.com>
2023-02-02 15:49:18 +0800

    test: scaling the property tests

--- src/mvba/consensus.rs
+++ src/mvba/consensus.rs
@@ -208,10 +208,10 @@ mod tests {
 
     use super::Consensus;
     use crate::mvba::{bundle::Outgoing, *};
-    use blsttc::SecretKeySet;
-    use rand::{thread_rng, Rng};
 
+    use blsttc::SecretKeySet;
     use quickcheck_macros::quickcheck;
+    use rand::{thread_rng, Rng, SeedableRng};
 
     fn valid_proposal(_id: NodeId, _: &Proposal) -> bool {
         true
@@ -258,26 +258,74 @@ mod tests {
 
     #[test]
     fn test_prevent_decision_conflict() {
-        do_prop_random_msg_delivery(239389896331702023721706851822556999354)
-    }
+        let seed: u128 = 239389896331702023721706851822556999354;
+        let mut seed_buf = [0u8; 32];
+        seed_buf[0..16].copy_from_slice(&seed.to_le_bytes());
+        let mut rng = rand::rngs::StdRng::from_seed(seed_buf);
 
-    #[test]
-    fn test_prevent_double_pre_vote() {
-        do_prop_random_msg_delivery(321606964118541147451444878468141021383)
+        let mut net = TestNet::new();
+
+        for c in &mut net.cons {
+            let proposal = (0..4).map(|_| rng.gen_range(0..64)).collect();
+            let mut msgs = c.propose(proposal).unwrap();
+            net.buffer.append(&mut msgs);
+        }
+
+        while !net.buffer.is_empty() {
+            let rand_index = rng.gen_range(0..net.buffer.len());
+            let rand_msg = &net.buffer.remove(rand_index);
+            let mut msgs = Vec::new();
+            log::trace!("random message: {:?}", rand_msg);
+
+            for c in &mut net.cons {
+                msgs.append(&mut match rand_msg {
+                    Outgoing::Direct(id, bundle) => {
+                        if id == &c.self_id {
+                            c.process_bundle(bundle).unwrap()
+                        } else {
+                            Vec::new()
+                        }
+                    }
+                    Outgoing::Gossip(bundle) => c.process_bundle(bundle).unwrap(),
+                });
+            }
+
+            net.buffer.append(&mut msgs);
+        }
+
+        let mut decisions = HashMap::new();
+        for c in &mut net.cons {
+            if c.decided_proposer.is_some() {
+                let value = c
+                    .abba_map
+                    .get(&c.decided_proposer.unwrap())
+                    .unwrap()
+                    .decided_value()
+                    .unwrap();
+
+                log::debug!(
+                    "test for consensus {} finished on proposal {} with {value}",
+                    c.self_id,
+                    c.decided_proposer.unwrap(),
+                );
+                decisions.insert(c.self_id, (c.decided_proposer.unwrap(), value));
+            }
+        }
+
+        // check if all consensus results are equal:
+        assert_eq!(decisions.len(), net.cons.len());
+        // https://sts10.github.io/2019/06/06/is-all-equal-function.html
+        let first = decisions.iter().next().unwrap().1;
+        assert!(decisions.iter().all(|(_, item)| item == first));
     }
 
     #[quickcheck]
     fn prop_random_msg_delivery(seed: u128) {
-        do_prop_random_msg_delivery(seed)
-    }
-
-    fn do_prop_random_msg_delivery(seed: u128) {
         let _ = env_logger::builder()
             .is_test(true)
             .filter_level(log::LevelFilter::Debug)
             .try_init();
 
-        use rand::SeedableRng;
         let mut seed_buf = [0u8; 32];
         seed_buf[0..16].copy_from_slice(&seed.to_le_bytes());
         let mut rng = rand::rngs::StdRng::from_seed(seed_buf);
